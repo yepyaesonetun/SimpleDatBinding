7f36d9951b322a6e51b526e9341f9d55e5c72f73
yepyaesonetunMMDS <yepyaesone.tun@mm-digital-solutions.com>
2020-02-14 10:00:20 +0630

    Initial commit

---
+++ app/src/main/java/com/padcmyanmar/padcx/simpledatbinding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.padcmyanmar.padcx.simpledatbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.padcmyanmar.padcx.simpledatbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val activityBinding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        activityBinding.tvName.text = "Ko Ye"
+        activityBinding.tvAddress.text = "Yangon, Myanmar"
+
+    }
+}
---
+++ app/src/main/java/com/padcmyanmar/padcx/simpledatbinding/UserVO.kt
@@ -0,0 +1,11 @@
+package com.padcmyanmar.padcx.simpledatbinding
+
+/**
+ * Created by Ye Pyae Sone Tun
+ * on 2020-02-14.
+ */
+
+class UserVO {
+    private var name: String? = null
+    private var address: String? = null
+}
